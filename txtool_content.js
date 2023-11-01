ce331b57cf34c33767f3734de3fed15f7ac3275f
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2023-11-01 17:29:00 +0100

    Update txtool_content.js

--- txtool_content.js
+++ txtool_content.js
@@ -1,11 +1,11 @@
-// contentScript.js
-
+// Sound variables definition
 const audioHome = new Audio(chrome.runtime.getURL('sounds/home.mp3'));
 const audioAlert = new Audio(chrome.runtime.getURL('sounds/alert.mp3'));
 const volume = 0.5;
 audioHome.volume = volume;
 audioAlert.volume = volume;
 
+// Function definition
 function isKill(text) { // if text suggest a kill
   if (text.includes('by') && text.includes('died')) {
     return true;
@@ -13,35 +13,34 @@ function isKill(text) { // if text suggest a kill
   return false;
 }
 
-// Detect if the page contains txAdmin
+// Main
+// Check if txAdmin is detected
 if (document.querySelector('meta[name="description"][content="txAdmin - remotely Manage & Monitor your GTA5 FiveM Server"]')) {
   audioHome.play();
   console.log('[txAdmin Tool] - txAdmin detected');
 }
-
+// Get element logContainer
 const logContainer = document.getElementById('logContainer');
-
+// Observer to watch for new log
 const observer = new MutationObserver((mutationsList) => {
   mutationsList.forEach((mutation) => {
     const addedText = mutation.addedNodes[0]?.textContent;
 
+    // Text added
     if (addedText) {
       if (addedText.includes('died') || addedText.includes('explosion')) {
-        
         const addedElement = mutation.addedNodes[0];
-        // format
+        // Format log line
         if (isKill(addedText)) {
           addedElement.style.backgroundColor = 'red';
           addedElement.style.border = '1px solid yellow';
-          // alert sound
+          // Play sound alert
           audioAlert.play();
         } else {
           addedElement.style.border = '1px solid red';
         }
-        // log
+        // Log entry in browser console
         console.log('[txAdmin Tool] - Alert : ' + addedText);
-        
-
       }
     }
   });
