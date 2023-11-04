7949d5ea79cd6ce7461215fe926f56a027c4126a
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2023-11-04 17:48:16 +0100

    Ajout de mots reconnus

--- txtool_content.js
+++ txtool_content.js
@@ -5,6 +5,14 @@ const volume = 0.5;
 audioHome.volume = volume;
 audioAlert.volume = volume;
 
+// Colors
+
+var color_vehicle = '#154360'; // dark blue
+var color_explosion = 'orange'; // orange
+var color_kill = 'red'; // red
+var color_death = '#4D5656'; // light grey
+var color_txadmin = '#9C640C'; // dark orange
+
 // Function definition
 function isKill(text) { // if text suggest a kill
   if (text.includes('by') && text.includes('died') && !text.includes('suicide')) {
@@ -32,17 +40,23 @@ const observer = new MutationObserver((mutationsList) => {
       if (addedText.includes('died')) {
         // Format log line
         if (isKill(addedText)) {
-          addedElement.style.backgroundColor = 'red';
+          addedElement.style.backgroundColor = color_kill;
           // Play sound alert
           audioAlert.play();
         } else {
-          addedElement.style.backgroundColor = 'grey';
+          addedElement.style.backgroundColor = color_death;
         }
         // Log entry in browser console
         console.log('[txAdmin Tool] - Death alert : ' + addedText);
       } else if (addedText.includes('explosion')){
-        addedElement.style.backgroundColor = 'orange';
+        addedElement.style.backgroundColor = color_explosion;
         console.log('[txAdmin Tool] - Explosion alert : ' + addedText);
+      } else if (addedText.includes('their vehicle')){
+        addedElement.style.backgroundColor = color_vehicle;
+        console.log('[txAdmin Tool] - Vehicle modification alert : ' + addedText);
+      } else if (addedText.includes('txAdmin')){
+        addedElement.style.backgroundColor = color_txadmin;
+        console.log('[txAdmin Tool] - txAdmin message : ' + addedText);
       }
     }
   });
